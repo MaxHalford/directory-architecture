b32a3f8ce0b48b3a7484a5ba313030496f86fb6e
MaxHalford <maxhalford25@gmail.com>
2015-06-03 23:10:38 +0200

    emptied files

--- examples/update.py
+++ examples/update.py
@@ -1,9 +1,1 @@
-from lib import JCDecaux as jcd
-import time
 
-city = 'Toulouse'
-
-while True:
-    dataframe = jcd.information(city)
-    dataframe.to_csv('static/data/{0}.csv'.format(city), index=False))
-    time.sleep(60)
